973a41d3b3dbfaa44c0ecf6bdfda1af2c462dc08
Mohammad Zakout <modi.zak.5@gmail.com>
2020-05-29 05:35:29 +0300

    Single User Details fragment

---
+++ app/src/main/java/com/funkycoders/myapplication/ui/UserDetailsFragment.java
@@ -0,0 +1,32 @@
+package com.funkycoders.myapplication.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.funkycoders.myapplication.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UserDetailsFragment extends Fragment {
+
+    public UserDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_user_details, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+}
